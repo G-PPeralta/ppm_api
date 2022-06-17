b579e0cb425d5ff6d3c62282f5bcbea8f3190eff
Alan Rente <alan.rente@iksolution.com.br>
2022-06-17 13:40:13 -0300

    fix: get user in validate

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,13 +1,14 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 import { jwtConstants } from 'auth/constants';
 import { Encrypt64 } from 'utils/security/encrypt.security';
 import { User } from '@prisma/client';
+import { UserService } from 'user/user.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private userService: UserService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -17,13 +18,16 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
   async validate(payload: any): Promise<User> {
     const { email, senha } = payload;
+    const user = await this.userService.findOneByEmail(email);
+
+    const { email: _email, senha: _senha } = user;
 
     const crypt = new Encrypt64();
-    if (!email) throw new BadRequestException('Nenhum login informado');
-    if (!senha) throw new BadRequestException('Nenhuma senha informada');
+    if (!_email) throw new BadRequestException('Nenhum login informado');
+    if (!_senha) throw new BadRequestException('Nenhuma senha informada');
     // if (!role) throw new BadRequestException('Nenhuma role informada');
 
-    const cryptedPassword = crypt.toBase64fromsha512(payload.senha);
-    return { senha: cryptedPassword, ...payload };
+    const cryptedPassword = crypt.toBase64fromsha512(senha);
+    return { _senha: cryptedPassword, ...payload };
   }
 }
