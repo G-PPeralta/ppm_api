1663f108fc2d6eaac442250312d245252df82b76
Alan Rente <alan.rente@iksolution.com.br>
2022-06-17 12:39:15 -0300

    fix: remove required token in signup

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -16,8 +16,6 @@ import { LocalAuthGuard } from 'auth/guards/local-auth.guard';
 import { JwtAuthGuard } from 'auth/guards/jwt-auth.guard';
 import { ApiBearerAuth } from '@nestjs/swagger';
 
-@ApiBearerAuth()
-@UseGuards(JwtAuthGuard)
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
@@ -30,30 +28,38 @@ export class UserController {
       return new InternalServerErrorException(error.message);
     }
   }
-
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
   @Get('pending')
   async findAllPending() {
     const users = await this.userService.findUsersProfilePending();
     return users;
   }
-
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
   @Get()
   async findAll() {
     const users = await this.userService.findAll();
     return UserMapper.mapToListUserShortenedDto(users);
   }
 
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   async findOne(@Param('id') id: string) {
     const user = await this.userService.findOne(+id);
     return UserMapper.mapToUserDto(user);
   }
 
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   update(@Param('id') id: string) {
     return this.userService.update(+id);
   }
 
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.userService.remove(+id);
