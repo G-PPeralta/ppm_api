bdac9b8283a64f13c86e5fb3844132e32d6f51e4
Alan Rente <alan.rente@iksolution.com.br>
2022-06-22 12:58:17 -0300

    fix: add resource roles

---
+++ src/roles/roles.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
+import { RolesService } from './roles.service';
+import { CreateRoleDto } from './dto/create-role.dto';
+import { UpdateRoleDto } from './dto/update-role.dto';
+import { JwtAuthGuard } from 'auth/guards/jwt-auth.guard';
+import { Roles } from 'auth/roles/roles.decorator';
+import { Perfil } from 'types/roles';
+
+@UseGuards(JwtAuthGuard)
+@Roles(Perfil.ADMIN)
+@Controller('roles')
+export class RolesController {
+  constructor(private readonly rolesService: RolesService) {}
+
+  @Post()
+  create(@Body() createRoleDto: CreateRoleDto) {
+    return this.rolesService.create(createRoleDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.rolesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.rolesService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateRoleDto: UpdateRoleDto) {
+    return this.rolesService.update(+id, updateRoleDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.rolesService.remove(+id);
+  }
+}
