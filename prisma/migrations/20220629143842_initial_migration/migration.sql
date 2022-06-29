9e22617135d21dfdcefd7efc00c8f40436ca6f4e
dev-luizf <luizfurtadomkt@hotmail.com>
2022-06-29 11:43:53 -0300

    refactor: update migration to adhere prisma models

--- prisma/migrations/20220629143842_initial_migration/migration.sql
+++ prisma/migrations/20220629143842_initial_migration/migration.sql
@@ -15,14 +15,36 @@ CREATE TABLE "tb_atividades" (
     "data_restricao_inicio" DATE,
     "data_restricao_depois" DATE,
     "responsavel_id" INTEGER NOT NULL,
+    "deletado" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "tb_atividades_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "rl_tb_desafios_tb_projetos" (
+    "desafio_id" INTEGER NOT NULL,
+    "projeto_id" INTEGER NOT NULL,
+    "deletado" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "rl_tb_desafios_tb_projetos_pkey" PRIMARY KEY ("desafio_id","projeto_id")
+);
+
+-- CreateTable
+CREATE TABLE "tb_desafios" (
+    "id" SERIAL NOT NULL,
+    "observacoes" VARCHAR(255) NOT NULL,
+    "data_inicio" DATE,
+    "data_fim" DATE NOT NULL,
+    "deletado" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "tb_desafios_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "tb_classificacoes_projetos" (
     "id" SERIAL NOT NULL,
     "classificacao" VARCHAR(45) NOT NULL,
+    "deletado" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "tb_classificacoes_projetos_pkey" PRIMARY KEY ("id")
 );
@@ -31,6 +53,7 @@ CREATE TABLE "tb_classificacoes_projetos" (
 CREATE TABLE "tb_complexidades" (
     "id" SERIAL NOT NULL,
     "complexidade" VARCHAR(45) NOT NULL,
+    "deletado" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "tb_complexidades_pkey" PRIMARY KEY ("id")
 );
@@ -39,6 +62,7 @@ CREATE TABLE "tb_complexidades" (
 CREATE TABLE "tb_demandas" (
     "id" SERIAL NOT NULL,
     "demanda" VARCHAR(45) NOT NULL,
+    "deletado" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "tb_demandas_pkey" PRIMARY KEY ("id")
 );
@@ -47,6 +71,7 @@ CREATE TABLE "tb_demandas" (
 CREATE TABLE "tb_divisoes_projetos" (
     "id" SERIAL NOT NULL,
     "divisao" VARCHAR(45) NOT NULL,
+    "deletado" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "tb_divisoes_projetos_pkey" PRIMARY KEY ("id")
 );
@@ -55,6 +80,7 @@ CREATE TABLE "tb_divisoes_projetos" (
 CREATE TABLE "tb_gates" (
     "id" SERIAL NOT NULL,
     "gate" VARCHAR(45) NOT NULL,
+    "deletado" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "tb_gates_pkey" PRIMARY KEY ("id")
 );
@@ -63,6 +89,7 @@ CREATE TABLE "tb_gates" (
 CREATE TABLE "tb_locais" (
     "id" SERIAL NOT NULL,
     "local" VARCHAR(45) NOT NULL,
+    "deletado" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "tb_locais_pkey" PRIMARY KEY ("id")
 );
@@ -72,6 +99,7 @@ CREATE TABLE "tb_logs" (
     "id" SERIAL NOT NULL,
     "log" JSONB NOT NULL,
     "data" TIMESTAMP(6) NOT NULL DEFAULT timezone('gmt3'::text, now()),
+    "deletado" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "tb_logs_pkey" PRIMARY KEY ("id")
 );
@@ -80,6 +108,7 @@ CREATE TABLE "tb_logs" (
 CREATE TABLE "tb_polos" (
     "id" SERIAL NOT NULL,
     "polo" VARCHAR(45) NOT NULL,
+    "deletado" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "tb_polos_pkey" PRIMARY KEY ("id")
 );
@@ -88,6 +117,7 @@ CREATE TABLE "tb_polos" (
 CREATE TABLE "tb_prioridades_projetos" (
     "id" SERIAL NOT NULL,
     "prioridade" VARCHAR(45) NOT NULL,
+    "deletado" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "tb_prioridades_projetos_pkey" PRIMARY KEY ("id")
 );
@@ -96,6 +126,7 @@ CREATE TABLE "tb_prioridades_projetos" (
 CREATE TABLE "tb_proj_ativ_predecessor" (
     "atividade_id" INTEGER NOT NULL,
     "predecessor_id" INTEGER NOT NULL,
+    "deletado" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "tb_proj_ativ_predecessor_pkey" PRIMARY KEY ("atividade_id","predecessor_id")
 );
@@ -106,6 +137,7 @@ CREATE TABLE "tb_proj_atv_historico" (
     "descricao" VARCHAR(255) NOT NULL,
     "data_historico" DATE,
     "atividade_id" INTEGER NOT NULL,
+    "deletado" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "tb_proj_atv_historico_pkey" PRIMARY KEY ("id")
 );
@@ -133,6 +165,7 @@ CREATE TABLE "tb_projetos" (
     "dataInicio_real" DATE,
     "dataFim_real" DATE,
     "comentarios" VARCHAR(255),
+    "deletado" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "tb_projetos_pkey" PRIMARY KEY ("id")
 );
@@ -142,6 +175,7 @@ CREATE TABLE "tb_roles" (
     "id" SERIAL NOT NULL,
     "role" VARCHAR NOT NULL,
     "nome_role" VARCHAR(50) NOT NULL,
+    "deletado" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "tb_roles_pkey" PRIMARY KEY ("id")
 );
@@ -150,6 +184,7 @@ CREATE TABLE "tb_roles" (
 CREATE TABLE "tb_solicitantes_projetos" (
     "id" SERIAL NOT NULL,
     "solicitante" VARCHAR(45) NOT NULL,
+    "deletado" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "tb_solicitantes_projetos_pkey" PRIMARY KEY ("id")
 );
@@ -158,6 +193,7 @@ CREATE TABLE "tb_solicitantes_projetos" (
 CREATE TABLE "tb_status_atividade" (
     "id" SERIAL NOT NULL,
     "status" VARCHAR(45) NOT NULL,
+    "deletado" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "tb_status_atividade_pkey" PRIMARY KEY ("id")
 );
@@ -166,6 +202,7 @@ CREATE TABLE "tb_status_atividade" (
 CREATE TABLE "tb_status_projetos" (
     "id" SERIAL NOT NULL,
     "status" VARCHAR(45) NOT NULL,
+    "deletado" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "tb_status_projetos_pkey" PRIMARY KEY ("id")
 );
@@ -174,6 +211,7 @@ CREATE TABLE "tb_status_projetos" (
 CREATE TABLE "tb_tipos_projeto" (
     "id" SERIAL NOT NULL,
     "tipo" VARCHAR(45) NOT NULL,
+    "deletado" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "tb_tipos_projeto_pkey" PRIMARY KEY ("id")
 );
@@ -187,15 +225,10 @@ CREATE TABLE "tb_usuarios" (
     "telefone" VARCHAR NOT NULL,
     "area_atuacao" VARCHAR NOT NULL,
     "role_id" INTEGER NOT NULL DEFAULT 2,
+    "deletado" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "tb_usuarios_pkey" PRIMARY KEY ("id")
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "tb_proj_ativ_predecessor_atividade_id_key" ON "tb_proj_ativ_predecessor"("atividade_id");
-
--- CreateIndex
-CREATE UNIQUE INDEX "tb_proj_ativ_predecessor_predecessor_id_key" ON "tb_proj_ativ_predecessor"("predecessor_id");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "tb_usuarios_email_key" ON "tb_usuarios"("email");
@@ -211,6 +244,12 @@ ALTER TABLE "tb_atividades" ADD CONSTRAINT "fk_tb_macroatividade_tb_status_acoes
 
 -- AddForeignKey
 ALTER TABLE "tb_atividades" ADD CONSTRAINT "fk_tb_atividades_tb_usuarios1" FOREIGN KEY ("responsavel_id") REFERENCES "tb_usuarios"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "rl_tb_desafios_tb_projetos" ADD CONSTRAINT "fk_tb_desafios_has_tb_projetos_tb_desafios1" FOREIGN KEY ("desafio_id") REFERENCES "tb_desafios"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "rl_tb_desafios_tb_projetos" ADD CONSTRAINT "fk_tb_desafios_has_tb_projetos_tb_projetos1" FOREIGN KEY ("projeto_id") REFERENCES "tb_projetos"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- AddForeignKey
 ALTER TABLE "tb_proj_ativ_predecessor" ADD CONSTRAINT "fk_tb_proj_ativ_predecessor_tb_atividades1" FOREIGN KEY ("atividade_id") REFERENCES "tb_atividades"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
