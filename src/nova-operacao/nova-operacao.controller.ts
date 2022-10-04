54225d5a90e77013b882f2b92e5ea80d613ae6ca
= <=>
2022-10-04 12:10:52 -0300

    feat: operacoes sonda e poco

--- src/nova-operacao/nova-operacao.controller.ts
+++ src/nova-operacao/nova-operacao.controller.ts
@@ -1,5 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateNovaOperacao } from './dto/create-nova-operacao.dto';
+import { CreatePoco } from './dto/create-poco.dto';
+import { CreateSonda } from './dto/create-sonda.dto';
 import { NovaOperacaoService } from './nova-operacao.service';
 
 @Controller('nova-operacao')
@@ -14,5 +16,25 @@ export class NovaOperacaoController {
   @Get()
   findAll() {
     return this.novaOperacaoService.findAll();
+  }
+
+  @Get('poco')
+  findPocos() {
+    return this.novaOperacaoService.findPoco();
+  }
+
+  @Get('sonda')
+  findSonda() {
+    return this.novaOperacaoService.findSonda();
+  }
+
+  @Post('poco')
+  createPoco(@Body() createPoco: CreatePoco) {
+    return this.novaOperacaoService.createPoco(createPoco);
+  }
+
+  @Post('sonda')
+  createSonda(@Body() createSonda: CreateSonda) {
+    return this.novaOperacaoService.createSonda(createSonda);
   }
 }
