90600584ac02f36761818f90ed7948cafa1333ae
Felipe Mateus <eu@felipemateus.com>
2022-10-11 11:16:32 -0300

    fix(fix projects routes): fix routes projects

--- src/budgets/budgets.service.ts
+++ src/budgets/budgets.service.ts
@@ -249,13 +249,13 @@ export class BudgetsService {
   }
 
   async findAllProjects() {
-    /*const projetos = await this.prisma.tb_projetos_atividade.findMany({
+    const projetos = await this.prisma.tb_projetos_atividade.findMany({
       select: { nom_atividade: true, id: true },
       where: { id_pai: 0 },
     });
 
     return projetos.map((data) => {
       return { nome: data.nom_atividade, id: data.id };
-    });*/
+    });
   }
 }
