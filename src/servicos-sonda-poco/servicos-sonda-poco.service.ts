5d3dcb5e47c3a331c3bbd2eeda7025f34b5f7aad
= <=>
2022-10-15 16:48:39 -0300

    fix: pocos e sondas

--- src/servicos-sonda-poco/servicos-sonda-poco.service.ts
+++ src/servicos-sonda-poco/servicos-sonda-poco.service.ts
@@ -7,30 +7,47 @@ export class ServicosSondaPocoService {
 
   async findSondas() {
     return await this.prisma.$queryRawUnsafe(`
-    select a.id, concat(a.id, ' - ', nome_projeto) as nom_sonda
-    from tb_projetos a
-    inner join tb_projetos_atividade b 
-        on a.id = b.id_projeto 
-    where 
-    b.id_pai = 0
-    and a.tipo_projeto_id = 3;`);
+    select id, nom_sonda, ordem from (
+      select a.id, concat(a.id, ' - ', nome_projeto) as nom_sonda, 1 as ordem
+          from tb_projetos a
+          inner join tb_projetos_atividade b 
+              on a.id = b.id_projeto 
+          where 
+          b.id_pai = 0
+          and a.tipo_projeto_id = 3
+      union 
+          select 0 as id, concat(0, ' - ', nom_sonda) as nom_sonda, 0 as ordem
+          from tb_sondas
+          where 
+              nom_sonda not in (select nome_projeto from tb_projetos tp where tipo_projeto_id = 3)
+      ) as qr
+  order by ordem desc;`);
   }
 
   async findPocos(id_projeto: number) {
     return await this.prisma.$queryRawUnsafe(`
-    select 
-    a.id, concat(a.id, ' - ', nom_atividade) as nom_poco,
-    (select 
-            --case when min(dat_ini_plan) - INTERVAL '45 DAY' < now() then now() else min(dat_ini_plan) - INTERVAL '45 DAY' end as dat_ini_plan,
-        min(dat_ini_plan) as dat_ini_limite
-        from tb_projetos_atividade b
-        where     
-            id_pai = a.id) as dat_ini_limite
-    from tb_projetos_atividade a  
-    where 
-        id_projeto = ${id_projeto}
-    and id_operacao is null
-    and id_pai <> 0;
+    select id, nom_poco, dat_ini_limite, ordem from (
+      select  a.id, concat(a.id, ' - ', nom_atividade) as nom_poco,
+          (select 
+              --case when min(dat_ini_plan) - INTERVAL '45 DAY' < now() then now() else min(dat_ini_plan) - INTERVAL '45 DAY' end as dat_ini_plan,
+              min(dat_ini_plan) as dat_ini_limite
+              from tb_projetos_atividade b
+              where     
+                  id_pai = a.id) as dat_ini_limite, 1 as ordem
+          from tb_projetos_atividade a  
+          where 
+              id_projeto = ${id_projeto}
+          and id_operacao is null
+          and id_pai <> 0
+      union 
+          select 0 as id, concat(0, ' - ', nom_poco) as nom_poco, 
+          (select max(dat_fim_real) from tb_projetos_atividade where id_projeto = 457 ) as dat_ini_limite, 
+          0 as ordem
+          from tb_pocos
+          where 
+              nom_poco not in (select nom_atividade from tb_projetos_atividade where pct_real < 100)
+      )as qr
+      order by ordem desc, dat_ini_limite asc
     `);
   }
 
