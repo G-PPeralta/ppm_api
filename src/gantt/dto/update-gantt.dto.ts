df63ec1eeea0ae6f9b521af72afd43ec90c35c95
G-PPeralta <prates_peralta@hotmail.com>
2022-10-20 11:06:35 -0300

    fix: remove datas planejadas do dto de update do gantt

--- src/gantt/dto/update-gantt.dto.ts
+++ src/gantt/dto/update-gantt.dto.ts
@@ -1,7 +1,7 @@
 export class UpdateGanttDto {
   dat_ini_real: string;
   dat_fim_real: string;
-  dat_ini_plan: string;
-  dat_fim_plan: string;
+  // dat_ini_plan: string;
+  // dat_fim_plan: string;
   pct_real: number;
 }
