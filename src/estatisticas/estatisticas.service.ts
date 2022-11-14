f5c6cd688277edf64bfbc441bdb301aa769719ea
G-PPeralta <prates_peralta@hotmail.com>
2022-11-14 15:34:14 -0300

    fix: corrige query get atividades gantt estatistica

--- src/estatisticas/estatisticas.service.ts
+++ src/estatisticas/estatisticas.service.ts
@@ -65,7 +65,7 @@ export class EstatisticasService {
               ) as calc
               on calc.nom_atividade = atividades.nom_atividade
         where
-        sonda.dat_usu_erase is null and
+        atividades.dat_usu_erase is null and
         sonda.id_pai = 0
         group by 
         sonda.id, sonda.nom_atividade, pocos.nom_atividade, pocos.id,
@@ -73,7 +73,6 @@ export class EstatisticasService {
         atividades.id, atividades.dat_ini_plan, atividades.dat_fim_plan, atividades.dat_ini_real, atividades.dat_ini_plan,
         responsaveis.nome_responsavel, vlr_min, vlr_max, vlr_dp, vlr_med
         order by atividades.id asc, atividades.dat_ini_real asc, atividades.id_pai asc;
-
     `);
 
     const tratamento = [];
@@ -233,7 +232,7 @@ export class EstatisticasService {
 
   async apagarAtividade(id: number) {
     await this.prisma.$queryRawUnsafe(`
-      DELETE FROM tb_projetos_atividade WHERE id = ${id}
+      UPDATE tb_projetos_atividade set dat_usu_erase = now() WHERE id = ${id}
     `);
   }
 }
